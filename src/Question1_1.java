ab3fb1e647dd980ab849edcdb29decc577f43388
nSwane <swane.nawaoui@gmail.com>
2015-01-25 19:43:14 +0100

    Word count

---
+++ src/Question1_1.java
@@ -0,0 +1,63 @@
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class Question1_1 {
+	public static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+		@Override
+		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			for (String word: value.toString().split("\\s+")){
+				context.write(new Text(word), new IntWritable(1));
+			}
+		}
+	}
+
+	public static class MyReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+		@Override
+		protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			long sum = 0;
+			for(IntWritable value: values){
+				sum += value.get();
+			}
+			context.write(key, new LongWritable(sum));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		String input = otherArgs[0];
+		String output = otherArgs[1];
+		Job job = Job.getInstance(conf, "Question1_1");
+		job.setJarByClass(Question1_1.class);
+		
+		job.setMapperClass(MyMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+
+		job.setReducerClass(MyReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(input));
+		job.setInputFormatClass(TextInputFormat.class);
+		
+		FileOutputFormat.setOutputPath(job, new Path(output));
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
